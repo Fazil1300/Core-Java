99714c617480290c3ba4a283f2d25ebe6f3de7a7
Md Fazil <mohamedfazil1300@gmail.com>
2023-01-03 18:42:18 +0530

    Iteration Statement II

---
+++ forLoop.java
@@ -0,0 +1,13 @@
+//for loop -> executes a block of code a limited amount of time
+public class forLoop {
+    public static void main(String[] args) {
+        for(int index=10;index<=0;index--){
+            System.out.println(index);
+        }
+        System.out.println("Happy New Year");
+    }
+}
+/*NOTES
+    Parts of for loop
+        for(intialization;condition;increment/decrement)
+ */
