be0325dcb70159f783cb2d7b63c96bbf42c83f44
Md Fazil <mohamedfazil1300@gmail.com>
2023-01-03 18:10:55 +0530

    Comments

---
+++ comments.java
@@ -0,0 +1,9 @@
+//Comments are used for understanding and documentation purpose only,They are two kinds
+/*Single line
+ *Multi line
+ */
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("Comments doesn't made changes to program");//Prints whatever inside dooble quotes
+    }
+}
