248f2aead45823881f2c501f2149f36974141970
Md Fazil <mohamedfazil1300@gmail.com>
2023-01-03 18:25:56 +0530

    Relational Operators

---
+++ relationalOperators.java
@@ -0,0 +1,20 @@
+/*Relational Operators
+ *      ==      Equal to
+ *      !=      Not equal to
+ *      >       Greater than
+ *      <       Lesser than
+ *      >=      Greater than or Equal to
+ *      <=      Lesser than or Equal to
+ */
+public class relationalOperators {
+   public static void main(String[] args) {
+    int a=1,b=2;
+    System.out.println("a:"+a+"\nb:"+b);
+    System.out.println("Equal to:"+(a==b));
+    System.out.println("Not Equal to:"+(a!=b));
+    System.out.println("Greater than:"+(a>b));
+    System.out.println("Lesser than:"+(a<b));
+    System.out.println("Greater than or Equal to:"+(a>=b));
+    System.out.println("Lesser than or Equal to:"+(a<=b));
+   } 
+}
