9e75a34cdd22c91c244f92d2979c2861d1041bda
Md Fazil <mohamedfazil1300@gmail.com>
2023-01-03 18:43:27 +0530

    Iteration Statement III

---
+++ enhancedforLoop.java
@@ -0,0 +1,9 @@
+public class enhancedforLoop {
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,4,5,6,7,8,9,10};
+        for(int n:numbers){
+            System.out.println(n);
+        }
+    }
+}
+//We can use Enhanced For Loop in Array,Vector,List
