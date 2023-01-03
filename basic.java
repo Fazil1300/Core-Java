c0c83ce94855faa7d1cac087b495ebe11a3d925f
Md Fazil <mohamedfazil1300@gmail.com>
2023-01-03 18:01:23 +0530

    First Java Program

--- basic.java
+++ basic.java
@@ -1,6 +1,8 @@
-
+//Basic Java Program
 public class basic {
     public static void main(String[] args) {
         System.out.println("Hello,World");
     }
 }
+/*Every java application begins with class defintion(Starting Class)
+  main() method is the entry point of the program*/
