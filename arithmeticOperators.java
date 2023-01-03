db81a2c33ad59f6eac8ebb0f8b31b1f84e006d17
Md Fazil <mohamedfazil1300@gmail.com>
2023-01-03 18:23:41 +0530

    Arithmetic Operators

---
+++ arithmeticOperators.java
@@ -0,0 +1,22 @@
+/*Arithmetic Operators
+ *  +   addition
+ *  -   subtraction
+ *  *   multiplication
+ *  /   division
+ *  %   modulus 
+ */
+public class arithmeticOperators {
+    public static void main(String[] args) {
+        int a=123,b=10;
+        System.out.println("a:"+a+"\nb:"+b);
+        System.out.println("Addition:"+(a+b));
+        System.out.println("Subtraction:"+(a-b));
+        System.out.println("Multiplication:"+(a*b));
+        System.out.println("Division:"+(a/b));
+        System.out.println("Modulus:"+(a%b));
+    }
+}
+/*Expression -> Operators+Operand
+  Operands   -> Values,Variables,Numbers,Quantity
+  Operators  -> A symbol having special duties to do
+*/
